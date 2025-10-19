f38a4f9f6dad4091a8f65dbf313cd5cd2e6cf83f
eliellmiranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2025-10-19 08:20:24 -0300

    refactor(ex009)
    replaced manual counter with range(1, 11) loop for cleaner and more efficient code in the multiplication table program

--- PYTHON/Gustavo Guanabara's Python course/ex009.py
+++ PYTHON/Gustavo Guanabara's Python course/ex009.py
@@ -1,13 +1,10 @@
 # Write a program that reads any integer and displays its multiplication table.
 
-print('-------------------------------------------')
+print('---------------------------')
 print('=== WELCOME TO THE MULTIPLICATION TABLE ===')
-print('-------------------------------------------')
+print('---------------------------')
 
 number = int(input('Enter the number you want to see the multiplication table for: '))
 
-counter = 1
-for i in range(10):
-   mult = number * counter
-   print("{} x {} = {}".format(number, counter, mult))
-   counter += 1
+for i in range(1,11):
+    print(f"{number} x {i} = {number * i}")
