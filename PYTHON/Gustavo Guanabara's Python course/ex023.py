ea58a490181403f401d1da398a98ba7aef10186c
eliellmiranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2025-10-29 10:52:39 -0300

    Update ex023.py

--- PYTHON/Gustavo Guanabara's Python course/ex023.py
+++ PYTHON/Gustavo Guanabara's Python course/ex023.py
@@ -2,17 +2,13 @@
 displays each of its digits separately.
 Example: 1834 → Unit: 4; Ten: 3; Hundred: 8; Thousand: 1 """
 
-number = input("Enter a number from 0 to 9999: ")
+number = int(input('Enter a number: '))
 
-length = len(number)
+unit = number // 1 % 10
+ten = number // 10 % 10
+hundred = number // 100 % 10
+thousand = number // 1000 % 10
 
-if length == 1:
-    print(f'Unit: {number[0]}')
-elif length == 2:
-    print(f'Unit: {number[1]}\nTen: {number[0]}')
-elif length == 3:
-    print(f'Unit: {number[2]}\nTen: {number[1]}\nHundred: {number[0]}')
-elif length == 4:
-    print(f'Unit: {number[3]}\nTen: {number[2]}\nHundred: {number[1]}\nThousand: {number[0]}')
-else:
-    print('Please enter a number between 0 and 9999!')
+print(f'The entered number was: {number}!')
+print('UNIT: {}\nTEN: {}\nHUNDRED: {}\nTHOUSAND: {}'.format(unit, ten, hundred, thousand))
+
